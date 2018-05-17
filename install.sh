6356eb893cedfd5456be4a04bd563f9e81bf34f2
jachim <jachim@macbook>
2018-05-17 22:30:06 +0200

    gsed

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 wget --no-check-certificate https://github.com/servisys/ispconfig_setup/archive/master.zip
 unzip master.zip
+sed="sed"
 if [[ "$OSTYPE" == "darwin"* ]]; then
     sed="gsed"
 fi
 find . -type f -print0 | xargs -0 $sed -i 's/php7.0/php7.1/g'
-gsed -i "s/cd \/tmp/#cd \/tmp/g" ispconfig_setup-master/distros/debian9/install_ispconfig.sh
+$sed -i "s/cd \/tmp/#cd \/tmp/g" ispconfig_setup-master/distros/debian9/install_ispconfig.sh
 search="cd ispconfig3_install\/install\/"
-gsed -i "s/$search/\.\.\/prepare_ispconfig\.sh\n$search\nls\nexit 1/g" ispconfig_setup-master/distros/debian9/install_ispconfig.sh
+$sed -i "s/$search/\.\.\/prepare_ispconfig\.sh\n$search\nls\nexit 1/g" ispconfig_setup-master/distros/debian9/install_ispconfig.sh
 cd ispconfig_setup-master
 ./install.sh
