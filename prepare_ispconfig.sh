bc77a680404aef6f5d5099d97e325680f99e01fc
jachim <jachim@macbook>
2018-05-18 08:27:54 +0200

    php7.1

--- prepare_ispconfig.sh
+++ prepare_ispconfig.sh
@@ -3,5 +3,5 @@ sed="sed"
 if [[ "$OSTYPE" == "darwin"* ]]; then
     sed="gsed"
 fi
-find ispconfig_setup-master -type f -print0 | xargs -0 $sed -i 's/php7.0/php7.1/g'
-find ispconfig_setup-master -type f -print0 | xargs -0 $sed -i 's/php\/7.0/php\/7.1/g'
+find ispconfig3_install -type f -print0 | xargs -0 $sed -i 's/php7.0/php7.1/g'
+find ispconfig3_install -type f -print0 | xargs -0 $sed -i 's/php\/7.0/php\/7.1/g'
