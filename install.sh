530a18a563aa41bcaf3cec79434939657a2b9596
jachim <jachim@macbook>
2018-05-18 08:03:16 +0200

    php7.1

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+./prepare.sh
 wget --no-check-certificate https://github.com/servisys/ispconfig_setup/archive/master.zip
 unzip master.zip
 sed="sed"
@@ -6,6 +7,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     sed="gsed"
 fi
 find . -type f -print0 | xargs -0 $sed -i 's/php7.0/php7.1/g'
+find . -type f -print0 | xargs -0 $sed -i 's/php\/7.0/php\/7.1/g'
 $sed -i "s/cd \/tmp/#cd \/tmp/g" ispconfig_setup-master/distros/debian9/install_ispconfig.sh
 search="cd ispconfig3_install\/install\/"
 $sed -i "s/$search/\.\.\/prepare_ispconfig\.sh\n$search\nls/g" ispconfig_setup-master/distros/debian9/install_ispconfig.sh
--- prepare_ispconfig.sh
+++ prepare_ispconfig.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-sudo apt-get install -y apt-transport-https lsb-release ca-certificates
-sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
-sudo apt-get update
 sed="sed"
 if [[ "$OSTYPE" == "darwin"* ]]; then
     sed="gsed"
 fi
 find . -type f -print0 | xargs -0 $sed -i 's/php7.0/php7.1/g'
+find . -type f -print0 | xargs -0 $sed -i 's/php\/7.0/php\/7.1/g'
